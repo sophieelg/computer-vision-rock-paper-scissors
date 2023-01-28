7b0a3c9526333093486adba078c2fa1b355daf05
sophieelg <sophie2592@hotmail.com>
2023-01-28 16:41:31 +0000

    get_computer_choice & get_user_choice defined

---
+++ manual_rps.py
@@ -0,0 +1,20 @@
+# %%
+import random
+
+# %%
+
+options = ['Rock', 'Paper', 'Scissors']
+
+def get_compter_choice():
+    computer_choice = random.choice(options)
+    return computer_choice
+
+def get_user_choice():
+    while True:
+        user_choice = input('Enter Rock, Paper or Scissors')
+        if user_choice not in options:
+            print('You must enter either Rock, Paper or Scissors exactly.')
+        else:
+            return user_choice
+
+# %%
