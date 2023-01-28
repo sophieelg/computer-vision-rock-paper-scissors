be2cd461aeee30384c848ad9f6be5870330e0a92
sophieelg <sophie2592@hotmail.com>
2023-01-28 17:51:42 +0000

    adds get_winner function

--- manual_rps.py
+++ manual_rps.py
@@ -16,4 +16,23 @@ def get_user_choice():
         else:
             return user_choice
 
+def get_winner(computer_choice, user_choice):
+    if computer_choice == user_choice:
+        print('It is a tie!')
+    elif user_choice == 'Rock':
+        if computer_choice == 'Paper':
+            print('You lost')
+        else:
+            print('You won!')
+    elif user_choice == 'Paper':
+        if computer_choice == 'Scissors':
+            print('You lost')
+        else:
+            print('You won!')
+    elif user_choice == 'Scissors':
+        if computer_choice == 'Rock':
+            print('You lost')
+        else:
+            print('You won!')
+
 # %%
