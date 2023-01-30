847e2fca2a141f2149b28336f96d2b59a2e6eb47
sophieelg <sophie2592@hotmail.com>
2023-01-30 18:20:49 +0000

    updating print message of loss

--- manual_rps.py
+++ manual_rps.py
@@ -24,17 +24,17 @@ def get_winner(computer_choice, user_choice):
         print('It is a tie!')
     elif user_choice == 'Rock':
         if computer_choice == 'Paper':
-            print('You lost.')
+            print('You lost')
         else:
             print('You won!')
     elif user_choice == 'Paper':
         if computer_choice == 'Scissors':
-            print('You lost.')
+            print('You lost')
         else:
             print('You won!')
     elif user_choice == 'Scissors':
         if computer_choice == 'Rock':
-            print('You lost.')
+            print('You lost')
         else:
             print('You won!')
 
