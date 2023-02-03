858efad1df37169f8562b236c1aa8bb0f5db09ee
sophieelg <sophie2592@hotmail.com>
2023-02-03 17:07:34 +0000

    adding text to camera image

--- camera_rps.py
+++ camera_rps.py
@@ -7,52 +7,68 @@ import numpy as np
 
 # %%
 
+model = load_model('keras_model.h5')
+options = ['Rock', 'Paper', 'Scissors', 'Nothing']
+
 class RockPaperScissors:
-    def __init__(self):
-        self.model = load_model('keras_model.h5')
-        self.cap = cv2.VideoCapture(0)
-        self.data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-        self.countdown_time = 10
-        self.start_time = time.time()
+    def __init__(self, model):
+        self.model = model
+        self.countdown_time = 5
+        self.computer_choice = random.choice(options)
         self.computer_wins = 0
         self.user_wins = 0
-        self.options = ['Rock', 'Paper', 'Scissors', 'Nothing']
     
     def get_prediction(self):
-        while True:    
-            ret, frame = self.cap.read()
+        cap = cv2.VideoCapture(0)
+        data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+        user_win_amount = "User wins: " + str(self.user_wins)
+        computer_win_amount = "Computer wins: " + str(self.computer_wins)
+        time_now = time.time()
+        while time.time() < time_now + self.countdown_time:    
+            ret, frame = cap.read()
             resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
             image_np = np.array(resized_frame)
             normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
-            self.data[0] = normalized_image
-            prediction = self.model.predict(self.data)
-            highest = np.argmax(prediction[0])
-            user_choice = self.options[highest]
-            current_time = time.time()
-            elapsed_time = current_time - self.start_time
-            remaining_time = self.countdown_time - elapsed_time
-            if remaining_time <= 0:
-                print(f'You choose {user_choice}.')
-                return user_choice         
+            data[0] = normalized_image
+            prediction = model.predict(data)
+            cv2.putText(frame, options[np.argmax(prediction)], (0, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 3)
+            cv2.putText(frame, user_win_amount, (0, 100), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 3)
+            cv2.putText(frame, computer_win_amount, (0, 150), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 3)
+            cv2.putText(frame, str(int((time_now + self.countdown_time - time.time()))), (0, 200), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 3)
             cv2.imshow('frame', frame)
             # Press q to close the window
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
         # After the loop release the cap object
-        self.cap.release()
+        cap.release()
         # Destroy all the windows
         cv2.destroyAllWindows()
 
-    def get_computer_choice(self):
-        computer = random.choice(self.options)
-        print(f'The computer chooses {computer}.')
-        return computer
+        highest = np.argmax(prediction[0])
+        user_choice = options[highest]
+        if user_choice == 'Rock':
+            print('You choose Rock.')
+            self.get_winner(user_choice)
+        elif user_choice == 'Paper':
+            print('You choose Paper.')
+            self.get_winner(user_choice)
+        elif user_choice == 'Scissors': 
+            print('You choose Scissors.')
+            self.get_winner(user_choice)
+        else:
+            print('You choose nothing.')
+        return (user_choice)
 
-    def get_winner(self, computer_choice, user_choice):
-        if computer_choice == user_choice:
+    def get_computer_choice(self):
+        self.computer_choice = random.choice(options)
+        print(f'The computer chooses {self.computer_choice}.')
+        return (self.computer_choice)
+
+    def get_winner(self, user_choice):
+        if self.computer_choice == user_choice:
             print('It is a tie!')
         elif user_choice == 'Rock':
-            if computer_choice == 'Paper':
+            if self.computer_choice == 'Paper':
                 print('You lost')
                 self.computer_wins += 1
                 print(f'Computer has won {self.computer_wins}.')
@@ -61,7 +77,7 @@ class RockPaperScissors:
                 self.user_wins += 1
                 print(f'You have won {self.user_wins}.')
         elif user_choice == 'Paper':
-            if computer_choice == 'Scissors':
+            if self.computer_choice == 'Scissors':
                 print('You lost')
                 self.computer_wins += 1
                 print(f'Computer has won {self.computer_wins}.')
@@ -70,7 +86,7 @@ class RockPaperScissors:
                 self.user_wins += 1
                 print(f'You have won {self.user_wins}.')
         elif user_choice == 'Scissors':
-            if computer_choice == 'Rock':
+            if self.computer_choice == 'Rock':
                 print('You lost')
                 self.computer_wins += 1
                 print(f'Computer has won {self.computer_wins}.')
@@ -79,20 +95,44 @@ class RockPaperScissors:
                 self.user_wins += 1
                 print(f'You have won {self.user_wins}.')
 
-def play_game():
-    game = RockPaperScissors()
-    computer_choice = game.get_computer_choice()
-    user_choice = game.get_prediction()
+    def countdown(self):
+        cap = cv2.VideoCapture(0)
+        start_time = time.time()
+        while time.time() < start_time + self.countdown_time:
+            ret, frame = cap.read()         
+            cv2.putText(frame, "Please choose between Rock, Paper or Scissors.", (50, 100), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 3)
+            cv2.imshow("frame", frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+def play_game(model):
+    game = RockPaperScissors(model)
+    game.countdown()
     while True:
         if game.computer_wins == 3:
             print('You lost! The computer won 3 times.')
-            break
+            restart = input('Press R to restart the game. Press Q to quit.')
+            if restart.lower() == "r":
+                game.computer_wins = 0
+                game.user_wins = 0
+                game.get_computer_choice()
+                game.get_prediction()
+            else:
+                break
         elif game.user_wins == 3:
             print('You won! You beat the computer 3 times.')
-            break
+            restart = input('Press R to restart the game. Press Q to quit.')
+            if restart.lower() == "r":
+                game.computer_wins = 0
+                game.user_wins = 0
+                game.get_computer_choice()
+                game.get_prediction()
+            else:
+                break
         else:
+            game.get_computer_choice()
             game.get_prediction()
-            game.get_winner(computer_choice, user_choice)
+            
 
-play_game()
+play_game(model)
 # %%
